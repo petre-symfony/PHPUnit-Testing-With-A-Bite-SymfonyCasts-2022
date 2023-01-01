cccf0467cd860b86edf339a61ea39def1a6f37ba
petrero <petrero@gmail.com>
2023-01-01 12:30:53 +0200

    3.5 Testing Class Methods - ./vendor/bin/phpunit --testdox
     Descriptive Testdox Output
     - Let me show you one more cool trick with PHPUnit. Move back to the terminal and run our tests again... but this time pass a --testdox flag:
    
     ./vendor/bin/phpunit --testdox
     And... Wooah! The output is different. Most importantly, it turned the method name into a human-readable sentence... which is minor, but cool.
    
     By the way, the phpunit executable has a lot more options and arguments available. Run PHPUnit with the help flag to see them.
    
     ./vendor/bin/phpunit --help
     We'll talk about the most useful of these throughout the tutorial.
    
     Before we keep going, we need to cleanup our test. Remove this testGreaterThan() assertion...
    
     and run our tests again:
    
     ./vendor/bin/phpunit --testdox
     And... YES! All of our tests are passing. Coming up next, we're going to get philosophical and take a look at Test Driven Development or simply - TDD.

--- tests/Unit/Entity/DinosaurTest.php
+++ tests/Unit/Entity/DinosaurTest.php
@@ -6,19 +6,14 @@ use App\Entity\Dinosaur;
 use PHPUnit\Framework\TestCase;
 
 class DinosaurTest extends TestCase {
-	public function testDinosaur(): void {
+	public function testCanGetAndSetData(): void {
 		$dino = new Dinosaur(
 			name: 'Big Eaty',
 			genus: 'Tyrannosaurus',
 			length: 15,
 			enclosure: 'Paddock A'
 		);
-
-		self::assertGreaterThan(
-			$dino->getLength(),
-			10,
-			message: 'Dino is supposed to be bigger than 10 meters!'
-		);
+		
 
 		self::assertSame('Big Eaty', $dino->getName());
 		self::assertSame('Tyrannosaurus', $dino->getGenus());
