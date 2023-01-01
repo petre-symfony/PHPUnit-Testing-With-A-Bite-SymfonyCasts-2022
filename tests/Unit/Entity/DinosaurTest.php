a46b4bc1c667747e7e35c6e7e67addb93cd5aaa9
petrero <petrero@gmail.com>
2023-01-01 11:35:59 +0200

    2.1 Our First Test
      We already have this Dinosaur class... and it's pretty simple. But when it comes to dinosaurs, bugs in our code can be, mmm, a bit painful. So let's add some basic tests!
    
      Creating the Test Class
      - Mmmm... where do we put this new test? We can technically put our tests anywhere within our project. But when we installed symfony/test-pack, Flex created a tests/ directory which, no surprise, is the recommended place to put our tests.
    
      Remember that, in this tutorial, we're only dealing with Unit tests. So, inside of tests/, create a new directory called Unit. And because our Dinosaur::class lives in the Entity namespace - create an Entity directory inside of that at the same time.
    
      All of this organization is technically optional: you can organize the tests/ directory however you want. But, putting all of our unit tests into a Unit directory is just... nice. And the reason we made the Entity directory is because we want the file structure inside of Unit to mirror our src/ directory structure. That's a best practice that keeps our tests organized.
    
      Finally, create a new class called DinosaurTest. Using that Test suffix makes sense: we're testing Dinosaur, so we call this DinosaurTest! But it's also a requirement: PHPUnit - our testing library - requires this. It also requires that each class extend TestCase
    
      Now let's go ahead and write a simple test to make sure everything is working.
    
      Inside our DinosaurTest class, let's add public function testIsWorks()... where we'll create the most exciting test ever! If you like return types - I do! - use void... though that's optional
    
     Inside call self::assertEquals(42, 42). That's it! It's not a very interesting test - if our computer thinks that 42 doesn't equal 42, we have bigger problems - but it's enough.
    
     Executing PHPUnit
     - How do we execute the test? By executing PHPUnit. At your terminal, run:
    
      ./vendor/bin/phpunit
     And... awesome! PHPUnit saw one test - for our one test method - and one assertion.
    
     We could also say bin/phpunit to execute our tests, which is basically just a shortcut to run vendor/bin/phpunit.
    
      But, I'm sure your curious... What's... an assertion?
    
      Looking back at DinosaurTest, the one assertion refers to the assertEquals() method, which comes from PHPUnit's TestCase class. If the actual value - 42 - doesn't match the expected value, the test would fail. PHPUnit has a bunch more assertion methods... and we can see them all by going to https://phpunit.readthedocs.io. This is full of goodies, including an "Assertions" section. And... wow! Look at them all... We'll talk about the most important assertions throughout the series. But for now, back to the test!

---
+++ tests/Unit/Entity/DinosaurTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\Unit\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class DinosaurTest extends TestCase {
+	public function testItWorks():void {
+		self::assertEquals(32, 32);
+	}
+}
