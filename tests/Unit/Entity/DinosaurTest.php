15855f34c8a9fc493e85e4285a8329ca92988224
petrero <petrero@gmail.com>
2023-01-01 11:53:07 +0200

    2.4 Our First Test
     Testing Failures 😱
     - What does it look like when a test fails? Let's find out! Change our expected 42 to a string inside testItWorks()... and do the same inside testItWorksTheSame(). Yup, one of these won't work.
    
     This time when we try it:
    
     ./vendor/bin/phpunit
    Oh no! One failure!
    
      DinosaurTest::testItWorksTheSame() failed asserting that 42 is identical to 42.
    
      So... assertEquals() passed, but assertSame() failed. That's because assertEquals() is the equivalent to doing an if 42 == 42: using the double equal sign. But assertSame() is equivalent to 42 === 42: with three equal signs.
    
     And since the string 42 does not triple-equals the integer 42, that test fails and PHPUnit yells at us.
    
     Ok, we've got our first tests behind us! Though... testing that the answer to life the universe and everything is equal to the answer to life the universe and everything... isn't very interesting. So next: let's write real tests for the Dinosaur class.

--- tests/Unit/Entity/DinosaurTest.php
+++ tests/Unit/Entity/DinosaurTest.php
@@ -6,10 +6,10 @@ use PHPUnit\Framework\TestCase;
 
 class DinosaurTest extends TestCase {
 	public function testItWorks():void {
-		self::assertEquals(32, 32);
+		self::assertEquals('32', 32);
 	}
 
 	public function testItWorksTheSame():void{
-		self::assertSame(42, 42);
+		self::assertSame('42', 42);
 	}
 }
