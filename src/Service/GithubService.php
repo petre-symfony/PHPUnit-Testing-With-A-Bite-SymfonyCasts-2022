040fbd14ea748424379c3f31bcd2e13b828f9aed
petrero <petrero@gmail.com>
2023-01-01 21:51:24 +0200

    9.2 9.2 GitHub Service: Implementation
     Inside of GithubService, instantiate an HTTP client with $client = HttpClient::create(). To make a request, call $client->request(). This needs 2 things. 1st: what HTTP method to use, like GET or POST. In this case, it should be GET. 2nd: the URL, which I'll paste in. This will fetch all of the "issues" from the dino-park repository via GitHub's API.
    
     Parse the HTTP Response
     - Ok, now what? Looking back at the dino-park repo, GitHub will return a JSON response that contains the issues we see here. Each issue has a title with a dino's name and if the issue has a label attached to it, we'll get that back too. So, set $client->request() to a new $response variable. Then, below, foreach() over $response->toArray() as an $issue. The cool thing about using Symfony's HTTP Client is that we don't have to bother transforming the JSON from GitHub into an array - toArray() does that heavy lifting for us. Inside this loop, check if the issue title contains the $dinosaurName. So if (str_contains(['title'], $dinosaurName)) then we'll // Do Something with that issue.
    
     At this point, we've found the issue for our dinosaur. Woo! Now we need to loop over each label to see if we can find the health status. To help, I'll paste in a private method: you can copy this from the code block on this page.
    
     This takes an array of labels... and when it finds one that starts with Status:, it returns the correct HealthStatus enum based on that label.
    
     Now instead of // Do Something, say $health = $this->getDinoStatusFromLabels() and pass the labels with $issue['labels'].

--- src/Service/GithubService.php
+++ src/Service/GithubService.php
@@ -3,13 +3,41 @@
 namespace App\Service;
 
 use App\Enum\HealthStatus;
+use Symfony\Component\HttpClient\HttpClient;
 
 class GithubService {
 	public function getHealthReport(string $dinosaurName): HealthStatus{
-		// Call GitHub API
+		$client = HttpClient::create();
 
-		// Filter the issues
+		$response = $client->request(
+			method: 'GET',
+			url: 'https://api.github.com/repos/SymfonyCasts/dino-park/issues'
+		);
 
-		return HealthStatus::HEALTHY;
+		foreach ($response->toArray() as $issue){
+			if (str_contains($issue['title'], $dinosaurName)){
+				$health = $this->getDinoStatusFomLabels($issue['labels']);
+			}
+		}
+
+		return $health;
+	}
+
+	private function getDinoStatusFomLabels(array $labels): HealthStatus{
+		$status = null;
+
+		foreach ($labels as $label){
+			$label = $label['name'];
+
+			// We only care about "Status" labels
+			if (!str_starts_with($label, 'Status:')) {
+				continue;
+			}
+
+			// Remove the "Status:" and whitespace from the label
+			$status = trim(substr($label, strlen('Status:')));
+		}
+
+		return HealthStatus::tryFrom($status);
 	}
 }
